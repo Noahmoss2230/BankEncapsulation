77b0d417454d41e9cdf32d40dae08dd40e9125a2
Noah Moss <noahmoss50@gmail.com>
2025-06-16 13:11:00 -0500

    Finished lesson

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BankEncapsulation
+﻿using System;
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var michaelsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            
+            michaelsAccount.Deposit(amountToDeposit);
+            
+            Console.WriteLine($"Thank you! Your balance is now {michaelsAccount.GetBalance()}.");
         }
     }
 }
